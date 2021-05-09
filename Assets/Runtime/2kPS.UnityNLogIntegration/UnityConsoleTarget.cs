7e41ce1e6ed40e670d8f2335e7ead5cbf5d1e365
Carlos DaLomba <carlos@2kpixelstudios.net>
2021-05-09 11:29:51 -0400

    Added support for Unity to switch Debug.LogXXX(...) method based on log level

--- Assets/Runtime/2kPS.UnityNLogIntegration/UnityConsoleTarget.cs
+++ Assets/Runtime/2kPS.UnityNLogIntegration/UnityConsoleTarget.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 using NLog;
 using NLog.Targets;
 using UnityEngine;
@@ -10,9 +13,61 @@ namespace PS2k.UnityNLogIntegration {
     //https://github.com/NLog/NLog/wiki/Register-your-custom-component
     [Target(nameof(UnityConsoleTarget), IsWrapper = true)]
     public class UnityConsoleTarget : Target {
+        private struct LogLevelInfo {
+            public LogLevel logLevel;
+            public Action<string> logFunction;
+        }
+        private static readonly Lazy<Dictionary<int, LogLevelInfo>> LogLookup = new Lazy<Dictionary<int, LogLevelInfo>>(
+            () => {
+                return new Dictionary<int, LogLevelInfo>() {
+                    { LogLevel.Trace.Ordinal,
+                        new LogLevelInfo() {
+                            logLevel = LogLevel.Trace,
+                            logFunction = Debug.Log
+                        }
+                    },
+                    { LogLevel.Debug.Ordinal,
+                        new LogLevelInfo() {
+                            logLevel = LogLevel.Debug,
+                            logFunction = Debug.Log
+                        }
+                    },
+                    { LogLevel.Info.Ordinal,
+                        new LogLevelInfo() {
+                            logLevel = LogLevel.Info,
+                            logFunction = Debug.Log
+                        }
+                    },
+                    { LogLevel.Warn.Ordinal,
+                        new LogLevelInfo() {
+                            logLevel = LogLevel.Warn,
+                            logFunction = Debug.LogWarning
+                        }
+                    },
+                    { LogLevel.Error.Ordinal,
+                        new LogLevelInfo() {
+                            logLevel = LogLevel.Error,
+                            logFunction = Debug.LogError
+                        }
+                    },
+                    { LogLevel.Fatal.Ordinal,
+                        new LogLevelInfo() {
+                            logLevel = LogLevel.Fatal,
+                            logFunction = Debug.LogError
+                        }
+                    },
+                };
+            }
+        );
+
         protected override void Write(LogEventInfo logEvent) {
             string message = logEvent.FormattedMessage;
-            Debug.Log(message);
+
+            LogLookup.Value[logEvent.Level.Ordinal].logFunction(message);
+
+            Exception e = logEvent.Exception;
+            if (e != null)
+                Debug.LogException(e);
         }
     }
 }
