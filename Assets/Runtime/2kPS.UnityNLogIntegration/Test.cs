ba5e03388133fb81a22b4af9647f526b3feeac8a
Carlos DaLomba <carlos@2kpixelstudios.net>
2021-05-08 23:44:50 -0400

    Added example MonoBehaviour to test out the custom UnityConsoleTarget

---
+++ Assets/Runtime/2kPS.UnityNLogIntegration/Test.cs
@@ -0,0 +1,24 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+using UnityEngine;
+
+using Logger = NLog.Logger;
+using ILogger = NLog.ILogger;
+
+namespace PS2k.UnityNLogIntegration {
+    public class Test : MonoBehaviour {
+        private void Start() {
+            LoggingConfiguration config = new LoggingConfiguration();
+
+            UnityConsoleTarget consoleTarget = new UnityConsoleTarget();
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, consoleTarget);
+            LogManager.Configuration = config;
+
+            Logger logger = config.LogFactory.GetLogger(GetType().FullName);
+            logger.Log(LogLevel.Info, "Omg!?");
+            logger.Log(LogLevel.Warn, "Omg!?");
+            logger.Log(LogLevel.Error, "Omg!?");
+        }
+    }
+}
