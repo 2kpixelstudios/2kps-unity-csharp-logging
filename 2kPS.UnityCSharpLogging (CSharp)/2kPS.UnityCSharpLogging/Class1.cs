87db960cadb937f63a486629bd27c7cce7ed792b
Carlos DaLomba <carlos@2kpixelstudios.net>
2021-05-09 14:20:02 -0400

    Added metadata to .csproj files, setup UnityCSharpLogger class

--- 2kPS.UnityCSharpLogging (CSharp)/2kPS.UnityCSharpLogging/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-namespace _2kPS.UnityCSharpLogging {
-    public class Class1 {
-    }
-}
---
+++ 2kPS.UnityCSharpLogging (CSharp)/2kPS.UnityCSharpLogging/UnityCSharpLogger.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PS2k.UnityCSharpLogging {
+    public class UnityCSharpLogger {
+        private static Lazy<UnityCSharpLogger> globalLogger = new Lazy<UnityCSharpLogger>(
+            () => new UnityCSharpLogger()
+        );
+    }
+}
